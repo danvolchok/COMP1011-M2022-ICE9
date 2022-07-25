e4f05b8a2adfd43a1bc1a66052163b6744e899c2
danvolchok <volchokdan@gmail.com>
2022-07-24 20:21:14 -0400

    added Movie Search Results class

---
+++ src/main/java/ca/georgiancollege/comp1011m2022ice9/MovieSearchResults.java
@@ -0,0 +1,38 @@
+package ca.georgiancollege.comp1011m2022ice9;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MovieSearchResults
+{
+    // private instance members
+
+    @SerializedName("Search")
+    private Movie[] m_movies;
+
+    @SerializedName("totalResults")
+    private String m_totalResults;
+
+    // Getters
+
+    public Movie[] getMovies() {
+        return m_movies;
+    }
+
+    public String getTotalResults() {
+        return m_totalResults;
+    }
+
+    // Overridden Methods
+
+
+    @Override
+    public String toString()
+    {
+        String movieList = "";
+        for (var movie: getMovies())
+        {
+            movieList += "Title: " + movie.getTitle() + ", IMBD ID: " + movie.getIMDB_ID() + "\n";
+        }
+        return movieList;
+    }
+}
